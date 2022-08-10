448d9a833cdf1541c745584bcde441bfe28db914
Jacob Perry <jacobperry@Jacob-P-MBA2.local>
2022-08-10 07:15:38 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//ONLY CHANGE THE ACCOUNT NUMBER VARIABLE HERE
+var accountNumber = 9967522;
+// MAKE NO CHANGES
+
+console.log(accountNumber);
+var _dcq = _dcq || [];
+var _dcs = { account: accountNumber, debug: true };
+
+function() {
+    var dc = document.createElement('script');
+    dc.type = 'text/javascript';
+    dc.async = true;
+    dc.src = 'https://tag.getdrip.com/' + accountNumber + '.js';
+    console.log(dc.src);
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(dc, s);
+}();
+
+
+function handle_submit(event) { // 3
+    event.preventDefault(); // 4
+    const output = {};
+    for (const [k, v] of (new FormData(event.target).entries())) { // 5
+        if (!!v) output[k] = v; // 6
+    }
+    _dcq.push(['identify', output]); // 7
+    event.target.submit(); // 8
+}
