5d62d11fb0006c8c55b5277cc4409c34a4a3162e
Jacob Perry <jacobperry@Jacob-P-MBA2.local>
2022-10-14 13:26:21 -0700

    API

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
- const client = require('drip-nodejs')({
-     token: "0672dc522d3b6827e4065c2866b59022",
-     accountId: 9967522
- });
+//ONLY CHANGE THE ACCOUNT NUMBER VARIABLE HERE
+var accountNumber = 9967522;
+// MAKE NO CHANGES
+
+console.log(accountNumber);
+var _dcq = _dcq || [];
+var _dcs = { account: accountNumber, debug: true };
+
+
+function fetchSub() {
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Basic 0672dc522d3b6827e4065c2866b59022','Access-Control-Allow-Origin','*', 'Origin','jacobnperry.github.io');
+    console.log(myHeaders.get('Authorization'));
+    var requestOptions = {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    fetch('https://api.getdrip.com/v2/9967522/subscribers', requestOptions)
+
+        .then(response => response.text())
+        .then(data => console.log(data));
+
+}
 
 
 
 
- var _dcq = _dcq || [];
- var _dcs = _dcs || {};
- _dcs.account = '9967522';
-
- (function() {
-     var dc = document.createElement('script');
-     dc.type = 'text/javascript';
-     dc.async = true;
-     dc.src = '//tag.getdrip.com/9967522.js';
-     var s = document.getElementsByTagName('script')[0];
-     s.parentNode.insertBefore(dc, s);
- })();
-
-
- window._dcq.push(['identify', { success: (resp) => console.log(resp) }]);
-
- window.dataLayer = window.dataLayer || [];
-
- function gtag() { dataLayer.push(arguments); }
- gtag('js', new Date());
-
- gtag('config', 'G-753PQ20YNC');
-
- gtag('event', 'visited page', { 'method': 'Google' });
- gtag('event', 'share', { 'method': 'Google' });
-
- function handle_submit(event) { // 3
-     event.preventDefault(); // 4
-     const output = {};
-     for (const [k, v] of (new FormData(event.target).entries())) { // 5
-         if (!!v) output[k] = v; // 6
-     }
-     _dcq.push(['identify', output]); // 7
-     event.target.submit(); // 8
- }
+(function() {
+    var dc = document.createElement('script');
+    dc.type = 'text/javascript';
+    dc.async = true;
+    dc.src = 'https://tag.getdrip.com/' + accountNumber + '.js';
+    console.log(dc.src);
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(dc, s);
+})();
 
 
 
- function fetchSub() {
-     const idOrEmail = "jacob.perry@drip.com";
 
-     client.fetchSubscriber(idOrEmail)
-         .then((response) => {
-             // Handle `response.body`
-         })
-         .catch((error) => {
-             // Handle errors
-         });
- }
+function handle_submit(event) { // 3
+    event.preventDefault(); // 4
+    const output = {};
+    for (const [k, v] of (new FormData(event.target).entries())) { // 5
+        if (!!v) output[k] = v; // 6
+    }
+    _dcq.push(['identify', output]); // 7
+    event.target.submit(); // 8
+}
+
+function apiPost() {
+    _dcq.push(["identify", {
+        email: "jacob.perry@drip.com",
+        success: function(response) {
+            // Call a method with the response object
+            // Success callback is optional
+        }
+    }]);
+
+    window._dcq.push(['track', 'Visited a page', { url: "window.location.href" }]);
+
+}
+
+window._dcq.push(['identify', { success: (resp) => console.log(resp) }]);
